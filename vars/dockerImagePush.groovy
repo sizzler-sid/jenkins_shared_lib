761f10d2bedee78f0602374d48ac7db5133dd475
sidgarg18 <siddharth.garg@publicissapient.com>
2024-03-13 14:27:09 +0530

    groovy file formatting

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -17,4 +17,4 @@ def call(String aws_account_id, String region, String ecr_repoName){
     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
-//}
+}
--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -12,4 +12,4 @@ def call(String aws_account_id, String region, String ecr_repoName){
     trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
     cat scan.txt
     """
-//}
+}
